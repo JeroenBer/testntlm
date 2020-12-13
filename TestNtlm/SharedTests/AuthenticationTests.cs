13f8c00780c30fb442f4fd4dc7a77ffc1076effd
jbeccardi <jeroen.bernsen@gmail.com>
2020-12-13 17:38:21 +0100

    TestNtlm start

---
+++ TestNtlm/SharedTests/AuthenticationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SharedTests
+{
+    public class AuthenticationTests
+    {
+        [Fact]
+        public async Task TestNtlmNetworkCredential()
+        {
+            var httpClientHandler = new HttpClientHandler()
+            {
+                UseCookies = false,
+                AutomaticDecompression = DecompressionMethods.GZip,
+                AllowAutoRedirect = true,
+            };
+
+            // Set credentials that will be sent to the server.
+            httpClientHandler.Credentials = new NetworkCredential("testuser", "Wh9nPWEA3Xsg", "testntlm");
+
+            var httpClient = new HttpClient(httpClientHandler);
+
+            var result = await httpClient.GetStringAsync("http://testntlm.westus2.cloudapp.azure.com/testntlm.htm");
+        }
+    }
+}
---
+++ TestNtlm/TestNtlm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestNtlm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
