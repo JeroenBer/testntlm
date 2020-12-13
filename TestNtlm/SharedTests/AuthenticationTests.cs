19f7452c6d663ccee8f82274fe4820ce2711ce5c
jbeccardi <jeroen.bernsen@gmail.com>
2020-12-13 17:59:40 +0100

    Added extra tests

--- TestNtlm/SharedTests/AuthenticationTests.cs
+++ TestNtlm/SharedTests/AuthenticationTests.cs
@@ -10,6 +10,11 @@ namespace SharedTests
 {
     public class AuthenticationTests
     {
+        public AuthenticationTests()
+        {
+            AppContext.SetSwitch("System.Net.Http.UseSocketsHttpHandler", false);
+        }
+
         [Fact]
         public async Task TestNtlmNetworkCredential()
         {
@@ -22,6 +27,31 @@ namespace SharedTests
 
             // Set credentials that will be sent to the server.
             httpClientHandler.Credentials = new NetworkCredential("testuser", "Wh9nPWEA3Xsg", "testntlm");
+
+            var httpClient = new HttpClient(httpClientHandler);
+
+            var result = await httpClient.GetStringAsync("http://testntlm.westus2.cloudapp.azure.com/testntlm.htm");
+        }
+
+        [Fact]
+        public async Task TestNtlmCredentialCache()
+        {
+            var httpClientHandler = new HttpClientHandler()
+            {
+                UseCookies = false,
+                AutomaticDecompression = DecompressionMethods.GZip,
+                AllowAutoRedirect = true,
+            };
+
+            // Set credentials that will be sent to the server.
+            httpClientHandler.Credentials = new CredentialCache
+            {
+                {
+                    new Uri("http://testntlm.westus2.cloudapp.azure.com"),
+                    "NTLM",
+                    new NetworkCredential("testuser", "Wh9nPWEA3Xsg", "testntlm")
+                }
+            };
 
             var httpClient = new HttpClient(httpClientHandler);
 
