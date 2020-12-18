02171e0f2ea31fc5af9d5e1f4681919284447249
jbeccardi <jeroen.bernsen@gmail.com>
2020-12-18 18:15:45 +0100

    Cleanup

--- TestNtlm/TestNtlmXamarinMacOs/Program.cs
+++ TestNtlm/TestNtlmXamarinMacOs/Program.cs
@@ -21,7 +21,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-using NUnitLite;
 
 namespace NUnitLite.Tests
 {
