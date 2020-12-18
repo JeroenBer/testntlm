404b283e5c1db4eb8aba043a801677958b7e47cc
jbeccardi <jeroen.bernsen@gmail.com>
2020-12-18 12:24:39 +0100

    XUnit working

---
+++ TestNtlm/TestNtlmAndroidXamarin/TestNtlmAndroidXamarin.Android/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xunit.Runners.UI;
+using Xunit.Sdk;
+
+namespace TestNtlmAndroidXamarin.Droid
+{
+    [Activity(Label = "xUnit Android Runner", MainLauncher = true, Theme = "@android:style/Theme.Material.Light")]
+    public class MainActivity : RunnerActivity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            // tests can be inside the main assembly
+            AddTestAssembly(Assembly.GetExecutingAssembly());
+
+            AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
+            // or in any reference assemblies			
+
+            //AddTestAssembly(typeof(PortableTests).Assembly);
+            // or in any assembly that you load (since JIT is available)
+
+#if false
+			// you can use the default or set your own custom writer (e.g. save to web site and tweet it ;-)
+			Writer = new TcpTextWriter ("10.0.1.2", 16384);
+			// start running the test suites as soon as the application is loaded
+			AutoStart = true;
+			// crash the application (to ensure it's ended) and return to springboard
+			TerminateAfterExecution = true;
+#endif
+
+            AutoStart = true;
+
+            // you cannot add more assemblies once calling base
+            base.OnCreate(bundle);
+        }
+    }
+}
